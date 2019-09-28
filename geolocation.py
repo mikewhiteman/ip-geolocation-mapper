ce4ca678d77282f3df025a1049f14366212a3657
Michael Whiteman <michael.whiteman@weightwatchers.com>
2019-09-27 21:59:02 -0400

    setting up basic structure

--- geolocation.py
+++ geolocation.py
@@ -5,3 +5,10 @@ Generates map visual based on IP geolocation from a list of IP addresses
 Author(s):
     Michael Whiteman (michael.s.whiteman@gmail.com)
 """
+
+
+def main():
+	pass
+
+if __name__== "__main__":
+  main()
