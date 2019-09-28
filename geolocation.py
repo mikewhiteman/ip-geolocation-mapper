7812ff5ebffe08caab6760978cf85f3ddbfc102e
Michael Whiteman <michael.whiteman@weightwatchers.com>
2019-09-27 22:19:35 -0400

    fixed spacing issue

--- geolocation.py
+++ geolocation.py
@@ -10,26 +10,26 @@ from geoip import geolite2
 
 
 def import_addresses():
-	targets = []
-	with open('ip_list.txt') as f:
-		for ip in f:
-			targets.append(ip)
-	return targets
+    targets = []
+    with open('ip_list.txt') as f:
+        for ip in f:
+            targets.append(ip)
+    return targets
 
 
 def geo_lookup(ip):
-	ip = ip.strip()
-	match = geolite2.lookup(ip)
-	if match:
-		return f"Found a match! {ip} is from {match.country}"
-	else:
-		return f"{ip} does not have a match in Geolite DB"
+    ip = ip.strip()
+    match = geolite2.lookup(ip)
+    if match:
+        return f"Found a match! {ip} is from {match.country}"
+    else:
+        return f"{ip} does not have a match in Geolite DB"
 
 
 def main():
-	ip_list = import_addresses()
-	for ip in ip_list:
-		print(geo_lookup(ip))
+    ip_list = import_addresses()
+    for ip in ip_list:
+        print(geo_lookup(ip))
 
 
 if __name__== "__main__":
