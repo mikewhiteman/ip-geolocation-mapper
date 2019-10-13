8508abe42c9a7eb67c13d040dff9795a0306fe67
Michael Whiteman <michael.whiteman@weightwatchers.com>
2019-10-12 22:20:30 -0400

    added comments

--- geolocation.py
+++ geolocation.py
@@ -12,6 +12,16 @@ import country_converter as coco
 
 
 def import_ip_addresses():
+
+    """
+    Imports IP addresses from text file for geomapping
+        Args:
+            None - will be set by arg later
+
+        Returns:
+            targets (list): List of IP addresses to geomap
+    """
+
     targets = []
     with open('ip_list.txt') as f:
         for ip in f:
@@ -20,6 +30,16 @@ def import_ip_addresses():
 
 
 def geo_lookup(ip_addresses):
+
+    """
+    Looks up IP addresses using the MaxMind geoIP database
+        Args:
+            ip_addresses (list)
+
+        Returns:
+            countries (list): List of ISO3 country codes
+    """
+
     countries = []
     for ip in ip_addresses:
         match = geolite2.lookup(ip.strip())
@@ -30,8 +50,21 @@ def geo_lookup(ip_addresses):
 
 
 
-# def generate_map(countries):
-#     data = pd.value_counts(countries).to_frame().reset_index()
+def generate_map(countries):
+    """
+    Generates Geopandas map using the converted ISO3 country codes
+        Args:
+            countries (list) - ISO3 country codes generated from MaxMind database lookup
+
+        Returns:
+            TBD
+    """
+
+
+    data = pd.value_counts(countries).to_frame().reset_index()
+    data.columns = ['Country',  'Count']
+    shapefile = 'data/ne_110m_admin_0_countries.shp'
+
 
 
 
